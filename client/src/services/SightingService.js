af808bf0a90dbebebcc6eec706b497754e953055
lauren brett <laurencareybrett@gmail.com>
2020-01-21 16:28:05 +0000

    mvp and exention done, full stack working, add, delete done

---
+++ client/src/services/SightingService.js
@@ -0,0 +1,26 @@
+const baseURL = 'http://localhost:3000/api/sightings/'
+
+export default {
+  getSightings(){
+    return fetch(baseURL)
+    .then(res => res.json())
+  },
+
+  postSighting(payload){
+    return fetch(baseURL, {
+      method: 'POST', 
+      body: JSON.stringify(payload),
+      headers: {'Content-Type': 'application/json'}
+    })
+      .then(res => {
+        return res.json()
+      })
+  }, 
+
+  deleteSighting(id){
+    return fetch(baseURL + id, {
+      method: 'DELETE'
+    })
+  }
+
+}
